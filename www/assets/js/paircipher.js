e15f885202bd7bbb1742172db4f9c5c80793f834
Yasmidrog <yasmidrog@gmail.com>
2019-01-07 22:30:06 +0300

    Update paircipher.js

--- www/assets/js/paircipher.js
+++ www/assets/js/paircipher.js
@@ -1,41 +1,62 @@
-let alpha="АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+let alpha = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 };
-function paircipher(key, phrase){
 
-    if(key.length>16)
+function crypt(key, phrase) {
+
+    if (key.length > 16)
         return null;
-    key=key.toUpperCase().trim();
-    phrase = phrase.toUpperCase().replace("Ё","Е");
-    let newAlpha=alpha+'';
-    for(let i=0;i<key.length; i++)
-        newAlpha=newAlpha.replace(key.charAt(i)+'', '');
+    key = key.toUpperCase().trim();
+    phrase = phrase.toUpperCase().replace("Ё", "Е");
+    let newAlpha = alpha + '';
+    for (let i = 0; i < key.length; i++)
+        newAlpha = newAlpha.replace(key.charAt(i) + '', '');
 
-    console.log(key+"\n"+newAlpha);
-    for(let i=0; i<phrase.length; i++){
-        let pos=key.indexOf(phrase.charAt(i)+'');
-        if(pos>-1){
-            phrase=phrase.replaceAt(i,newAlpha.charAt(pos)+'');
+    console.log("cr: "+ key + "\n" + newAlpha);
+    for (let i = 0; i < phrase.length; i++) {
+        let pos = key.indexOf(phrase.charAt(i) + '');
+        if (pos > -1) {
+            phrase = phrase.replaceAt(i, newAlpha.charAt(pos) + '');
+        }else{
+            let pos = newAlpha.indexOf(phrase.charAt(i) + '');
+            if(pos>-1)
+                phrase = phrase.replaceAt(i, key.charAt(pos) + '');
         }
     }
     return phrase;
 }
-$(document).ready(function (e) {
-    document.getElementById("process").disabled = true;
-    $("#key").keyup(function (e) {
 
-        e.target.value=e.target.value.replace(/[^а-яё]/ig,'');
-        if(e.target.value.length>16)
-            e.target.value=e.target.value.substr(0, 16);
-        e.target.value=Array.from(new Set(e.target.value)).join('');
-        $("#counter").html(e.target.value.length+"/16")
-        if(e.target.value.length==16)
-            document.getElementById("process").disabled = false;
-        else  document.getElementById("process").disabled = true;
+
+$(document).ready(function (e) {
+
+    document.getElementById("decrypt").disabled = true;
+    document.getElementById("crypt").disabled = true;
+
+    $("#key").keyup(function (e) {
+        e.target.value = e.target.value.replace(/[^а-яё]/ig, '');
+        if (e.target.value.length > 16)
+            e.target.value = e.target.value.substr(0, 16);
+        e.target.value = Array.from(new Set(e.target.value)).join('');
+        $("#counter").html(e.target.value.length + "/16")
+        if (e.target.value.length == 16) {
+            document.getElementById("crypt").disabled = false;
+            document.getElementById("decrypt").disabled = false;
+
+        }
+        else {
+            document.getElementById("crypt").disabled = true;
+            document.getElementById("decrypt").disabled = true;
+
+        }
 
     })
-    $("#process").click(function () {
-        $("#result").val(paircipher($("#key").val(), $("#phrase").val()))
+
+
+    $("#crypt").click(function () {
+            $("#result").val(crypt($("#key").val(), $("#phrase").val()))
+    })
+    $("#decrypt").click(function () {
+        $("#phrase").val(crypt($("#key").val(), $("#result").val()))
     })
 });
