bcabbd40d3957eeea68657ebfb4ed8e016d1917e
Mark <mark-smirnow@ya.ru>
2019-01-09 22:25:47 +0300

    add site

--- www/constructor.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('frame').src = "generator_reshetok.html";
-var button = document.getElementById('reload');
-// console.log(button);
-function refr()
-{
-	document.getElementById('frame').src = document.getElementById('frame').src;
-}
-button.addEventListener("click", refr);
